e0a03708075d5b8e361d9db6e769ea89540e7368
munnabysl <mehedi.munna@byslglobal.com>
2023-10-31 02:36:09 +0600

    modification seat builder

--- resources/views/seat-builder/builder.blade.php
+++ resources/views/seat-builder/builder.blade.php
@@ -25,11 +25,19 @@
                     @endforeach
                 </select>
 
+                <select onchange="addTable(this)" id="tbl" class="btn btn-primary">
+                    <option selected disabled>+ &#9711; Table</option>
+                    <option value="1">1 Seat Table</option>
+                    <option value="2">2 Seats Table</option>
+                    <option value="3">3 Seats Table</option>
+                    <option value="4">4 Seats Table</option>
+                    <option value="6">6 Seats Table</option>
+                    <option value="8">8 Seats Table</option>
+                    <option value="10">10 Seats Table</option>
+                    <option value="12">12 Seats Table</option>
+                    <option value="16">16 Seats Table</option>
+                </select>
 
-                <button class="btn btn-primary rectangle">+ &#9647; Table</button>
-                <button class="btn btn-primary circle">+ &#9711; Table</button>
-                <button class="btn btn-primary triangle">+ &#9651; Table</button>
-                <button class="btn btn-primary chair">+ Chair</button>
                 <button class="btn btn-danger remove">Remove</button>
                 <button onclick="buildSeat()" class="btn btn-success remove">Save</button>
                 <!--
@@ -89,367 +97,24 @@
     <script>
         let buildData = {
             floor_id: null,
-            canvasData: [],
             items: []
         };
         let canvas
-        let number
-        const grid = 30
-        const backgroundColor = '#f8f8f8'
-        const lineStroke = '#ebebeb'
-        const tableFill = 'rgba(150, 111, 51, 0.7)'
-        const tableStroke = '#694d23'
-        const tableShadow = 'rgba(0, 0, 0, 0.4) 3px 3px 7px'
-        const chairFill = 'rgba(67, 42, 4, 0.7)'
-        const chairStroke = '#32230b'
-        const chairShadow = 'rgba(0, 0, 0, 0.4) 3px 3px 7px'
-        const barFill = 'rgba(0, 93, 127, 0.7)'
-        const barStroke = '#003e54'
-        const barShadow = 'rgba(0, 0, 0, 0.4) 3px 3px 7px'
-        const barText = 'Bar'
-        const wallFill = 'rgba(136, 136, 136, 0.7)'
-        const wallStroke = '#686868'
-        const wallShadow = 'rgba(0, 0, 0, 0.4) 5px 5px 20px'
 
-        let widthEl = 812
-        let heightEl = 812
+
         let canvasEl = document.getElementById('canvas')
 
-        function initCanvas() {
-            if (canvas) {
-                canvas.clear()
-                canvas.dispose()
-            }
 
-            canvas = new fabric.Canvas('canvas')
-            number = 1
-            canvas.backgroundColor = backgroundColor
-
-            for (let i = 0; i < (canvas.height / grid); i++) {
-                const lineX = new fabric.Line([0, i * grid, canvas.height, i * grid], {
-                    stroke: lineStroke,
-                    selectable: false,
-                    type: 'line'
-                })
-                const lineY = new fabric.Line([i * grid, 0, i * grid, canvas.height], {
-                    stroke: lineStroke,
-                    selectable: false,
-                    type: 'line'
-                })
-                sendLinesToBack()
-                canvas.add(lineX)
-                canvas.add(lineY)
-            }
-
-            canvas.on('object:moving', function (e) {
-                snapToGrid(e.target)
-            })
-
-            canvas.on('object:scaling', function (e) {
-                if (e.target.scaleX > 5) {
-                    e.target.scaleX = 5
-                }
-                if (e.target.scaleY > 5) {
-                    e.target.scaleY = 5
-                }
-                if (!e.target.strokeWidthUnscaled && e.target.strokeWidth) {
-                    e.target.strokeWidthUnscaled = e.target.strokeWidth
-                }
-                if (e.target.strokeWidthUnscaled) {
-                    e.target.strokeWidth = e.target.strokeWidthUnscaled / e.target.scaleX
-                    if (e.target.strokeWidth === e.target.strokeWidthUnscaled) {
-                        e.target.strokeWidth = e.target.strokeWidthUnscaled / e.target.scaleY
-                    }
-                }
-            })
-
-            canvas.on('object:modified', function (e) {
-                e.target.scaleX = e.target.scaleX >= 0.25 ? (Math.round(e.target.scaleX * 2) / 2) : 0.5
-                e.target.scaleY = e.target.scaleY >= 0.25 ? (Math.round(e.target.scaleY * 2) / 2) : 0.5
-                snapToGrid(e.target)
-                if (e.target.type === 'table') {
-                    canvas.bringToFront(e.target)
-                } else {
-                    canvas.sendToBack(e.target)
-                }
-                sendLinesToBack()
-            })
-
-            canvas.observe('object:moving', function (e) {
-                checkBoudningBox(e)
-            })
-            canvas.observe('object:rotating', function (e) {
-                checkBoudningBox(e)
-            })
-            canvas.observe('object:scaling', function (e) {
-                checkBoudningBox(e)
-            })
-        }
-
-        initCanvas()
-
-        function resizeCanvas() {
-            canvasEl.width = 812
-            canvasEl.height = 812
-            const canvasContainerEl = document.querySelectorAll('.canvas-container')[0]
-            canvasContainerEl.style.width = canvasEl.width
-            canvasContainerEl.style.height = canvasEl.height
-        }
-
-        resizeCanvas()
-
-
-        function generateId() {
-            return Math.random().toString(36).substr(2, 8)
-        }
-
-        function addRect(left, top, width, height) {
-            const id = generateId()
-            const o = new fabric.Rect({
-                width: width,
-                height: height,
-                fill: tableFill,
-                stroke: tableStroke,
-                strokeWidth: 2,
-                shadow: tableShadow,
-                originX: 'center',
-                originY: 'center',
-                centeredRotation: true,
-                snapAngle: 45,
-                selectable: true
-            })
-            const t = new fabric.IText(number.toString(), {
-                fontFamily: 'Calibri',
-                fontSize: 14,
-                fill: '#fff',
-                textAlign: 'center',
-                originX: 'center',
-                originY: 'center'
-            })
-            const g = new fabric.Group([o, t], {
-                left: left,
-                top: top,
-                centeredRotation: true,
-                snapAngle: 45,
-                selectable: true,
-                type: 'table',
-                id: id,
-                number: number
-            })
-            canvas.add(g)
-            number++
-            return g
-        }
-
-        function addCircle(left, top, radius) {
-            const id = generateId()
-            const o = new fabric.Circle({
-                radius: radius,
-                fill: tableFill,
-                stroke: tableStroke,
-                strokeWidth: 2,
-                shadow: tableShadow,
-                originX: 'center',
-                originY: 'center',
-                centeredRotation: true
-            })
-            const t = new fabric.IText(number.toString(), {
-                fontFamily: 'Calibri',
-                fontSize: 14,
-                fill: '#fff',
-                textAlign: 'center',
-                originX: 'center',
-                originY: 'center'
-            })
-            const g = new fabric.Group([o, t], {
-                left: left,
-                top: top,
-                centeredRotation: true,
-                snapAngle: 45,
-                selectable: true,
-                type: 'table',
-                id: id,
-                number: number
-            })
-            canvas.add(g)
-            number++
-            return g
-        }
-
-        function addTriangle(left, top, radius) {
-            const id = generateId()
-            const o = new fabric.Triangle({
-                radius: radius,
-                fill: tableFill,
-                stroke: tableStroke,
-                strokeWidth: 2,
-                shadow: tableShadow,
-                originX: 'center',
-                originY: 'center',
-                centeredRotation: true
-            })
-            const t = new fabric.IText(number.toString(), {
-                fontFamily: 'Calibri',
-                fontSize: 14,
-                fill: '#fff',
-                textAlign: 'center',
-                originX: 'center',
-                originY: 'center'
-            })
-            const g = new fabric.Group([o, t], {
-                left: left,
-                top: top,
-                centeredRotation: true,
-                snapAngle: 45,
-                selectable: true,
-                type: 'table',
-                id: id,
-                number: number
-            })
-            canvas.add(g)
-            number++;
-            return g
-        }
-
-        function addChair(left, top, width, height) {
-            const o = new fabric.Rect({
-                left: left,
-                top: top,
-                width: 30,
-                height: 30,
-                fill: chairFill,
-                stroke: chairStroke,
-                strokeWidth: 2,
-                shadow: chairShadow,
-                originX: 'left',
-                originY: 'top',
-                centeredRotation: true,
-                snapAngle: 45,
-                selectable: true,
-                type: 'chair',
-                id: generateId()
-            })
-            canvas.add(o)
-            return o
-        }
-
-        function addBar(left, top, width, height) {
-            const o = new fabric.Rect({
-                width: width,
-                height: height,
-                fill: barFill,
-                stroke: barStroke,
-                strokeWidth: 2,
-                shadow: barShadow,
-                originX: 'center',
-                originY: 'center',
-                type: 'bar',
-                id: generateId()
-            })
-            const t = new fabric.IText(barText, {
-                fontFamily: 'Calibri',
-                fontSize: 14,
-                fill: '#fff',
-                textAlign: 'center',
-                originX: 'center',
-                originY: 'center'
-            })
-            const g = new fabric.Group([o, t], {
-                left: left,
-                top: top,
-                centeredRotation: true,
-                snapAngle: 45,
-                selectable: true,
-                type: 'bar'
-            })
-            canvas.add(g)
-            return g
-        }
-
-        function addWall(left, top, width, height) {
-            const o = new fabric.Rect({
-                left: left,
-                top: top,
-                width: width,
-                height: height,
-                fill: wallFill,
-                stroke: wallStroke,
-                strokeWidth: 2,
-                shadow: wallShadow,
-                originX: 'left',
-                originY: 'top',
-                centeredRotation: true,
-                snapAngle: 45,
-                selectable: true,
-                type: 'wall',
-                id: generateId()
-            })
-            canvas.add(o)
-            return o
-        }
-
-        function snapToGrid(target) {
-            buildData.items.push({id:target.id,no:target.number,type:target.type,top:target.top})
-            target.set({
-                left: Math.round(target.left / (grid / 2)) * grid / 2,
-                top: Math.round(target.top / (grid / 2)) * grid / 2
-            })
-        }
-
-        function checkBoudningBox(e) {
-            const obj = e.target
-
-            if (!obj) {
-                return
-            }
-            obj.setCoords()
-
-            const objBoundingBox = obj.getBoundingRect()
-            if (objBoundingBox.top < 0) {
-                obj.set('top', 0)
-                obj.setCoords()
-            }
-            if (objBoundingBox.left > canvas.width - objBoundingBox.width) {
-                obj.set('left', canvas.width - objBoundingBox.width)
-                obj.setCoords()
-            }
-            if (objBoundingBox.top > canvas.height - objBoundingBox.height) {
-                obj.set('top', canvas.height - objBoundingBox.height)
-                obj.setCoords()
-            }
-            if (objBoundingBox.left < 0) {
-                obj.set('left', 0)
-                obj.setCoords()
-            }
-        }
-
-        function sendLinesToBack() {
-            canvas.getObjects().map(o => {
-                if (o.type === 'line') {
-                    canvas.sendToBack(o)
-                }
-            })
-        }
-
-        document.querySelectorAll('.rectangle')[0].addEventListener('click', function () {
-            const o = addRect(0, 0, 60, 60)
-            canvas.setActiveObject(o)
-        })
-
-        document.querySelectorAll('.circle')[0].addEventListener('click', function () {
-            const o = addCircle(0, 0, 30)
-            canvas.setActiveObject(o)
-        })
-
-        document.querySelectorAll('.triangle')[0].addEventListener('click', function () {
-            const o = addTriangle(0, 0, 30)
-            canvas.setActiveObject(o)
-        })
-
-        document.querySelectorAll('.chair')[0].addEventListener('click', function () {
-            const o = addChair(0, 0)
-            canvas.setActiveObject(o)
-        })
+        canvas = new fabric.Canvas('canvas')
+        /*
+        fabric.Image.fromURL("{{asset('assets/seat-builder/')}}", function(img) {
+            // add background image
+            canvas.setBackgroundImage(img, canvas.renderAll.bind(canvas), {
+                //scaleX: canvas.width / img.width,
+                scaleY: canvas.height / img.height/2.5
+            });
+        });
+        */
 
 
         document.querySelectorAll('.remove')[0].addEventListener('click', function () {
@@ -461,6 +126,8 @@
                 canvas.renderAll()
             }
         })
+
+        /*
 
         document.querySelectorAll('.customer-mode')[0].addEventListener('click', function () {
             canvas.getObjects().map(o => {
@@ -505,15 +172,15 @@
 
         addDefaultObjects();
 
-        function setFloorId(t)
-        {
+         */
+
+        function setFloorId(t) {
             buildData.floor_id = t.value;
             getBuildData();
         }
 
 
         function buildSeat() {
-            //canvas = null;
             let selectedFloor = $('#floor').val();
             if (selectedFloor < 1) {
                 alert('please select floor first.Then try to save.')
@@ -532,8 +199,8 @@
                 })
             }
         }
-        function getBuildData()
-        {
+
+        function getBuildData() {
             let floor_id = $('#floor').val();
 
             $.ajax({
@@ -541,15 +208,9 @@
                 type: 'GET',
                 data: {floor_id: floor_id},
                 success: function (res) {
-                    canvas.clear();
-                    res.data.map(function (obj,key){
-                        let item =  JSON.parse(obj.canvas_obj);
-                        if(obj.type == 'table') {
-                            addTriangle(0,parseInt(item.top));
-                        }
-                        else if(obj.type == 'chair') {
-                            addChair(0,parseInt(item.top))
-                        }
+                    res.data.map(function (obj, key) {
+                        let item = JSON.parse(obj.canvas_obj);
+                        canvas.add(item);
                         console.log(obj)
                     })
                 },
@@ -557,6 +218,54 @@
                     console.log(res)
                 }
             })
+        }
+
+
+        function addTable(t) {
+            let tblImgUrl = "../assets/seat-builder/tbl" + t.value + ".png";
+
+            fabric.Image.fromURL(tblImgUrl, function (img) {
+                var oImg = img.set({left: 0, top: 0}).scale(.75);
+                let text = new fabric.Text("Table with " + t.value + ' Seat', {
+                    left: 10,
+                    top: 150,
+                    fontSize: 15,
+                    fill: 'black'
+                });
+                let title = new fabric.Text(prompt("Please enter table name/code:", ""), {
+                    left: 10,
+                    top: 170,
+                    fontSize: 15,
+                    fill: 'black'
+                });
+                let group = new fabric.Group([oImg, text, title],{
+                    id: generateId(),
+                    title: title
+                })
+                let obj = canvas.add(group);
+            });
+
+        }
+
+
+        canvas.on('object:modified', function (e) {
+            pushItemForStore(e)
+
+        })
+
+        canvas.on('object:store', function (e) {
+            pushItemForStore(e)
+        })
+
+        function pushItemForStore(e) {
+            let scaledObject = e.target;
+            buildData.items.push(scaledObject)
+            console.log('Width =  ' + scaledObject.getWidth());
+            console.log('Height = ' + scaledObject.getHeight());
+        }
+
+        function generateId() {
+            return Math.random().toString(36).substr(2, 8)
         }
 
     </script>
